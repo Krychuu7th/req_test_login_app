8d1581a8537aca1e3b208f677fb4983a4cdf3e77
Krychuu7th <krychuu7th@gmail.com>
2020-07-29 12:45:55 +0200

    Utworzenie aplikacji wysyłanej statusy do web serwisu podczas logowania

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'login-form';
+  logins: string[] = ['login', 'login2', 'login3', 'login4'];
+  passwords: string[] = ['pass', 'pass2', 'pass3', 'pass4'];
+
+  login: string = '';
+  password: string = '';
+  hide = true;
+  indexOfLogins: number;
+  indexOfPasswords: number;
+
+  invalidLogin: boolean;
+  validLogin:boolean;
+  errorMessage: string;
+
+  guid = 'a8961058-a6ee-4d71-b325-9aca83b22237';
+  xmlrequest: XMLHttpRequest;
+  params: string;
+
+  logInto() {
+    this.indexOfLogins = this.logins.indexOf(this.login);
+    this.indexOfPasswords = this.passwords.indexOf(this.password);
+    console.log(this.login);
+    console.log(this.indexOfLogins);
+    console.log(this.password);
+    console.log(this.indexOfPasswords);
+    if(this.indexOfLogins == -1 && this.indexOfPasswords == -1){
+      this.invalidLogin = true;
+      this.validLogin = false;
+      this.errorMessage = 'Invalid login or password';
+      this.sendRequest();
+    } else {
+      if (this.indexOfLogins == this.indexOfPasswords) {
+        this.login = '';
+        this.password = '';
+        this.invalidLogin = false;
+        this.validLogin = true;
+        this.errorMessage = 'LOGGED IN';
+        this.sendRequest();
+      }
+      else {
+        this.invalidLogin = true;
+        this.validLogin = false;
+        this.errorMessage = 'Invalid login or password';
+        this.sendRequest();
+      }
+    }
+  }
+
+  sendRequest(){
+    this.xmlrequest = new XMLHttpRequest();
+    this.params = `<?xml version="1.0" encoding="utf-8"?>
+                      <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
+                                     xmlns:std="http://prktices6.pl/serwis">
+                        <soap:Body>
+                          <std:sendStatus>
+                            <guid>${this.guid}</guid>
+                            <status>${this.validLogin}</status>
+                          </std:sendStatus>
+                        </soap:Body>
+                      </soap:Envelope>`;
+
+    this.xmlrequest.open("POST",
+      "http://localhost:9090/ws/applicationStatus");
+
+    this.xmlrequest.setRequestHeader('Content-type',
+      'text/xml');
+
+    this.xmlrequest.send(this.params);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
